ea5c1549f9928dc29a404acf20b8b9a060097eba
jbrajkovic <brajkovic.josip96@gmail.com>
2020-04-16 17:32:38 +0200

    first commit desktop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class="home">
+  <div v-if="mobile" class="home">
     <Pokedex />
+  </div>
+  <div v-else class="home-desktop">
+    HOME
   </div>
 </template>
 
 <script>
 import Pokedex from "@/components/Mobile/Pokedex/Pokedex.vue";
+import { isMobile } from 'mobile-device-detect';
+
 
 export default {
   name: "Home",
   components: {
     Pokedex
+  },
+  computed: {
+    mobile() {
+      return isMobile
+    }
   }
 };
 </script>
